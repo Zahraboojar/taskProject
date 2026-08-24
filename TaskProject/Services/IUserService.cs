fda4e647dbdd58d957dd75a1a462a38c9d09b576
zahraboojar <zahraboojar866@gmail.com>
2026-08-24 23:45:15 +0330

    add user service and interface

--- TaskProject/Services/IUserService.cs
+++ TaskProject/Services/IUserService.cs
@@ -1,10 +1,14 @@
-﻿using TaskProject.Models;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using TaskProject.Models;
 
 namespace TaskProject.Services
 {
     public interface IUserService
     {
         public Task<int> InsertUser(User user);
+        public Task<User> GetUser(int? id);
+        public Task<List<User>> GetAll();
+        public List<SelectListItem> GetAllSelcted(List<User> Users);
         public Task<int> UpdateUser(int id, User user);
         public Task<int> DeleteUser(int id);
     }
---
+++ TaskProject/Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TaskProject.Models;
+
+namespace TaskProject.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly TaskDbContext _context;
+
+        public UserService(TaskDbContext context)
+        {
+            _context = context;
+        }
+        public Task<int> DeleteUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<User>> GetAll()
+        {
+            return await _context.Users
+                .FromSqlInterpolated($"EXEC dbo.SelectUsers")
+                .ToListAsync();
+        }
+
+        public List<SelectListItem> GetAllSelcted(List<User> Users)
+        {
+            return Users.Select(c => new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = $"{c.FullName} ( {c.NationalCode} )"
+            }).ToList();
+        }
+
+        public Task<User> GetUser(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateUser(int id, User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
