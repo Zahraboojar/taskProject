c09dd902c8389202c898655767893d722101c048
zahraboojar <zahraboojar866@gmail.com>
2026-08-24 21:55:41 +0330

    add BaseFilterViewModel

---
+++ TaskProject/ViewModels/BaseFilterViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskProject.ViewModels
+{
+    public class BaseFilterViewModel
+    {
+        public string? SortColumn { get; set; }
+        public bool SortDescending { get; set; }
+
+        public int Page { get; set; } = 0;
+        public int ItemCount { get; set; } = 2;
+
+        public int TotalItems { get; set; }
+
+        public int TotalPages { get; set; }
+    }
+}
---
+++ TaskProject/ViewModels/UserFilterViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskProject.ViewModels
+{
+    public class UserFilterViewModel : BaseFilterViewModel
+    {
+        public string? FullName { get; set; }
+        public string? NationalCode { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
+        public string? Username { get; set; }
+    }
+}
