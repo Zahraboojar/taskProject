93acdbd9810ab2f1b0f47e77aee93c7e26c26690
zahraboojar <zahraboojar866@gmail.com>
2026-08-24 22:11:24 +0330

    add TaskUserService file and implement Interface Methods

---
+++ TaskProject/Services/TaskUserService.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TaskProject.Models;
+
+namespace TaskProject.Services
+{
+    public class TaskUserService : ITaskUserService
+    {
+        private readonly TaskDbContext _context;
+
+        public TaskUserService(TaskDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> DeleteTaskUserByTaskId(int taskId)
+        {
+            return _context.Database.ExecuteSqlInterpolatedAsync(
+                        $"EXEC dbo.DeleteTaskUserByTaskID {taskId}");
+        }
+
+        public Task<int> DeleteTaskUserByUserId(int userId)
+        {
+            return _context.Database.ExecuteSqlInterpolatedAsync(
+                        $"EXEC dbo.DeleteTaskUserByUserID {userId}");
+        }
+
+        public Task<int> InsertTaskUser(int taskId, int userId)
+        {
+            return _context.Database.ExecuteSqlInterpolatedAsync(
+                       $"EXEC dbo.InsertTaskUser {userId}, {taskId}");
+        }
+    }
+}
