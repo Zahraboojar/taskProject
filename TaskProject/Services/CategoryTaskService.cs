1533f5bbe2c20ab72303492b14745b026b72b8dd
zahraboojar <zahraboojar866@gmail.com>
2026-08-24 22:05:34 +0330

    add categoryTaskService file and implement Interface Methods

---
+++ TaskProject/Services/CategoryTaskService.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TaskProject.Models;
+
+namespace TaskProject.Services
+{
+    public class CategoryTaskService : ICategoryTaskService
+    {
+        private readonly TaskDbContext _context;
+
+        public CategoryTaskService(TaskDbContext context)
+        {
+            _context = context;
+        }
+        public Task<int> DeleteCategoryTaskByCategoryId(int categoryId)
+        {
+            return _context.Database.ExecuteSqlInterpolatedAsync(
+                        $"EXEC dbo.DeleteCategoryTaskByCategoryID {categoryId}");
+        }
+
+        public Task<int> DeleteCategoryTaskByTaskId(int taskId)
+        {
+            return _context.Database.ExecuteSqlInterpolatedAsync(
+                        $"EXEC dbo.DeleteCategoryTaskByTaskIDTask {taskId}");
+        }
+
+        public Task<int> InsertCategoryTask(int taskId, int categoryId)
+        {
+            return _context.Database.ExecuteSqlInterpolatedAsync(
+                        $"EXEC dbo.InsertCategoryTask {categoryId},{taskId}");
+        }
+    }
+}
