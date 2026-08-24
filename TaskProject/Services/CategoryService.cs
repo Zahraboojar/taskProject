933c5915a9eb6f0631755da46576a2605e225b87
zahraboojar <zahraboojar866@gmail.com>
2026-08-24 23:44:58 +0330

    add categoryService and interface

---
+++ TaskProject/Services/CategoryService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TaskProject.Models;
+
+namespace TaskProject.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly TaskDbContext _context;
+
+        public CategoryService(TaskDbContext context)
+        {
+            _context = context;
+        }
+        public Task<int> DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Category>> GetAll()
+        {
+            return await _context.Categories
+                .FromSqlInterpolated($"EXEC dbo.SelectCategories")
+                .ToListAsync();
+        }
+
+        public List<SelectListItem> GetAllSelcted(List<Category> categories)
+        {
+           return categories.Select(c => new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = c.Title
+            }).ToList();
+        }
+
+        public Task<Category> GetCategory(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateCategory(int id, Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TaskProject/Services/ICategoryService.cs
+++ TaskProject/Services/ICategoryService.cs
@@ -1,10 +1,14 @@
-﻿using TaskProject.Models;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using TaskProject.Models;
 
 namespace TaskProject.Services
 {
     public interface ICategoryService
     {
         public Task<int> InsertCategory(Category category);
+        public Task<Category> GetCategory(int? id);
+        public Task<List<Category>> GetAll();
+        public List<SelectListItem> GetAllSelcted(List<Category> categories);
         public Task<int> UpdateCategory(int id, Category category);
         public Task<int> DeleteCategory(int id);
     }
